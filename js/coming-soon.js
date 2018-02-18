f3c922e17f9e5e3ce88e28877c685deb3f29fe11
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-02-18 14:10:38 -0800

    creating project
    This is a project based developed from the BlackrockDigital/startbootstrap-coming-soon.
    Coming soon page for PL blog: kasiaikod.pl. The files have been loaded to Wordpress CMS by WP plugin.

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "http://www.kasiaikod.pl/wp-content/plugins/coming-soon/themes/kasi/mp4/Working-it.mp4",
+    poster: "http://www.kasiaikod.pl/wp-content/plugins/coming-soon/themes/kasi/img/bg-mobile-fallback.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
